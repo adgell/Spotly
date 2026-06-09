e894f1b3a128a85491e8e477d64add1b7ee49d0e
Ellina <ec5200@nyu.edu>
2026-06-09 15:34:58 +0300

    make navbar taller and logo much larger

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -12,16 +12,15 @@ export default function Navbar() {
     <header className="fixed top-0 left-0 right-0 z-50 bg-white/95 backdrop-blur-sm border-b border-black/8">
       <div className="max-w-[1400px] mx-auto px-4 sm:px-8">
         {/* Main row: flex with justify-between */}
-        <div className="flex items-center justify-between h-16">
-          {/* Logo - left */}
-          <Link href="/" className="flex items-center shrink-0">
-  <img
-    src="/spotly-logo.png"
-    alt="Spotly logo"
-    className="h-10 sm:h-12 md:h-14 w-auto"
-  />
-</Link>
-
+        <div className="flex items-center justify-between h-20">
+  {/* Logo - much larger */}
+  <Link href="/" className="flex items-center shrink-0">
+    <img
+      src="/spotly-logo.png"
+      alt="Spotly logo"
+      className="h-12 sm:h-14 md:h-16 w-auto"
+    />
+  </Link>
           {/* Desktop center nav - hidden on mobile */}
           <nav className="hidden md:flex items-center justify-center gap-8 absolute left-1/2 transform -translate-x-1/2">
             <Link href="/explore" className="text-sm text-black/60 hover:text-black transition-colors relative after:absolute after:bottom-[-2px] after:left-0 after:w-0 after:h-[1.5px] after:bg-black after:transition-all hover:after:w-full">
@@ -64,6 +63,7 @@ export default function Navbar() {
               </Link>
             </div>
           </div>
+        </div>
         </div>
       )}
     </header>
